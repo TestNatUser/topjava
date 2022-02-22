1a418a99580e5a2631fb94fc4297bbfb834c0917
Natalia Furmanova <nfurmanova@tula.co>
2022-02-22 12:09:33 +0300

    Apply 3_01_HW2_repositories.patch

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
@@ -1,55 +1,55 @@
 package ru.javawebinar.topjava.repository.inmemory;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 @Repository
 public class InMemoryUserRepository implements UserRepository {
-    private static final Logger log = LoggerFactory.getLogger(InMemoryUserRepository.class);
-    private final Map<Integer, User> repository = new ConcurrentHashMap<>();
+    public static final int USER_ID = 1;
+    public static final int ADMIN_ID = 2;
+    private final Map<Integer, User> usersMap = new ConcurrentHashMap<>();
     private final AtomicInteger counter = new AtomicInteger(0);
-
-
-    @Override
-    public boolean delete(int id) {
-        log.info("delete {}", id);
-        return repository.remove(id) != null;
-    }
 
     @Override
     public User save(User user) {
-        log.info("save {}", user);
         if (user.isNew()) {
             user.setId(counter.incrementAndGet());
-            repository.put(user.getId(), user);
+            usersMap.put(user.getId(), user);
             return user;
         }
-        return repository.computeIfPresent(user.getId(), (id, oldUser) -> user);
+        return usersMap.computeIfPresent(user.getId(), (id, oldUser) -> user);
     }
 
     @Override
+public boolean delete(int id) {
+         return usersMap.remove(id) != null;
+        }
+
+    @Override
     public User get(int id) {
-        log.info("get {}", id);
-        return repository.get(id);
+        return usersMap.get(id);
     }
 
     @Override
     public List<User> getAll() {
-        log.info("getAll");
-        return (List<User>) repository.values();
+        return usersMap.values().stream()
+                .sorted(Comparator.comparing(User::getName).thenComparing(User::getEmail))
+                .collect(Collectors.toList());
     }
 
     @Override
     public User getByEmail(String email) {
-        log.info("getByEmail {}", email);
-        return repository.get(email);
+        return usersMap.values().stream()
+                .filter(u -> email.equals(u.getEmail()))
+                .findFirst()
+                .orElse(null);
     }
 }
