a365847ab08b0032d3718359fdde513f07448849
Natalia Furmanova <nfurmanova@tula.co>
2022-05-31 12:45:34 +0300

    HW3

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,40 +1,71 @@
 package ru.javawebinar.topjava.model;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
+@NamedQueries({
+        @NamedQuery(name = Meal.DELETE, query = "DELETE FROM Meal m WHERE m.id=:id and m.user.id=:user_id"),
+        @NamedQuery(name = Meal.ALL_SORTED, query = "SELECT m FROM Meal m WHERE m.user.id=:user_id ORDER BY m.datetime desc"),
+        @NamedQuery(name = Meal.DATE_BETWEEN, query = "SELECT m FROM Meal m where m.user.id=:user_id and m.datetime >=:date_time and m.datetime <:date_time"),
+        @NamedQuery(name = Meal.GET_MEAL, query = "SELECT m FROM Meal m where m.user.id=:user_id and m.id=:id"),
+})
+
+@Entity
+@Table(name="meals",uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id", "date_time"}, name = "meals_unique_user_datetime_idx")})
 public class Meal extends AbstractBaseEntity{
-    private Integer id;
+    public static final String DELETE = "Meal.delete";
+    public static final String ALL_SORTED = "Meal.getAllSorted";
+    public static final String DATE_BETWEEN = "Meal.getDateBetween";
+    public static final String GET_MEAL = "Meal.getMeal";
 
-    private final LocalDateTime dateTime;
+    @Column(name = "date_time",nullable = false)
+    private  LocalDateTime datetime;
 
-    private final String description;
+    @Column(name = "description", nullable = false)
+    @Size(max = 128)
+    private  String description;
 
-    private final int calories;
+    @Column(name = "calories", nullable = false)
+    @NotBlank
+    @Size(max = 128)
+    private  int calories;
 
-    public Meal(LocalDateTime dateTime, String description, int calories) {
-        this(null, dateTime, description, calories);
+    public void setUser(User user) {
+        this.user = user;
     }
 
-    public Meal(Integer id, LocalDateTime dateTime, String description, int calories) {
+    @ManyToOne (targetEntity = User.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    @NotBlank
+    @Size(max = 128)
+    public User user;
+
+    public Meal(LocalDateTime datetime, String description, int calories) {
+        this(null, datetime, description, calories);
+    }
+
+    public Meal(Integer id, LocalDateTime datetime, String description, int calories) {
         super(id);
-        this.id = id;
-        this.dateTime = dateTime;
+        this.datetime = datetime;
         this.description = description;
         this.calories = calories;
     }
 
-    public Integer getId() {
-        return id;
+    public Meal(){
+
     }
 
-    public void setId(Integer id) {
-        this.id = id;
+    public Meal(Meal m){
+        this(m.id,m.datetime,m.description,m.calories);
     }
 
-    public LocalDateTime getDateTime() {
-        return dateTime;
+
+    public LocalDateTime getDatetime() {
+        return datetime;
     }
 
     public String getDescription() {
@@ -46,11 +77,12 @@ public class Meal extends AbstractBaseEntity{
     }
 
     public LocalDate getDate() {
-        return dateTime.toLocalDate();
+
+        return datetime.toLocalDate();
     }
 
     public LocalTime getTime() {
-        return dateTime.toLocalTime();
+        return datetime.toLocalTime();
     }
 
     public boolean isNew() {
@@ -61,9 +93,21 @@ public class Meal extends AbstractBaseEntity{
     public String toString() {
         return "Meal{" +
                 "id=" + id +
-                ", dateTime=" + dateTime +
+                ", datetime=" + datetime +
                 ", description='" + description + '\'' +
                 ", calories=" + calories +
                 '}';
+    }
+
+    public void setDatetime(LocalDateTime datetime) {
+        this.datetime = datetime;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
     }
 }
